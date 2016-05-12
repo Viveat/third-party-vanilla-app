c9d762ddb078ab698ee837445f50550f7bdda915
Francesco Pezzella <franzpezzella@gmail.com>
2016-05-12 18:19:49 +0200

    Importer route

--- client/config/router.js
+++ client/config/router.js
@@ -2,3 +2,9 @@ Router.configure({
   layoutTemplate: 'basicLayout',
   notFoundTemplate: 'notFound'
 });
+
+Router.route('/admin/importer', {
+  name: 'importer',
+  layoutTemplate: 'orionSemanticLayout',
+  template: 'importer'
+});
